eb01ecb9f7b5cccb29008a2f2bb45c9322592750
José Escamilla <54334921+Jose-Escamilla@users.noreply.github.com>
2025-04-10 17:26:56 -0600

    Update schedule-emails.js
    updated the body of the email

--- schedule-emails.js
+++ schedule-emails.js
@@ -3,16 +3,17 @@ function sendEmails() {
     var subject = "Oportunidad de Empleo";
   
     // Cuerpo del correo
-    var body = "Espero que este mensaje le encuentre bien. Mi nombre es José Escamilla, soy ingeniero mecatrónico de formación y he cursado una maestría en Ciencias de la Computación, aunque no la concluí por razones personales. Actualmente estoy deseoso de seguir aprendiendo y adquiriendo experiencia laboral en un entorno dinámico.\n\n" +
-               "En mi búsqueda activa de nuevas oportunidades laborales, estoy especialmente interesado en unirme a su equipo para aportar mi experiencia y seguir desarrollándome profesionalmente en un entorno desafiante.\n\n" +
-               "Adjunto a este correo encontrará mi currículum vitae, donde detallo mi historial académico y mi experiencia profesional. Estoy disponible para una entrevista en el momento que le resulte más conveniente y estaré encantado de proporcionar cualquier información adicional que considere necesaria.\n\n" +
-               "Agradezco de antemano su tiempo y atención. Quedo a la espera de su respuesta.\n\n" +
-               "Atentamente,\nIng. José Escamilla\nTel: 477XXXXXXX";
+    var body = "Espero que este mensaje le encuentre bien. Mi nombre es José Escamilla, soy ingeniero mecatrónico con formación complementaria en Ciencias de la Computación. Aunque no concluí la maestría por motivos personales, esta experiencia fortaleció mis habilidades técnicas y mi enfoque multidisciplinario.\n\n" +
+           "Actualmente me encuentro en búsqueda activa de oportunidades laborales donde pueda contribuir con mis conocimientos en ingeniería, programación y ciberseguridad, así como seguir creciendo profesionalmente en un entorno dinámico y desafiante.\n\n" +
+           "Adjunto a este correo encontrará mi currículum vitae, el cual detalla mi trayectoria académica, experiencia docente, proyectos de investigación y certificaciones recientes. Estoy disponible para una entrevista en el momento que le resulte conveniente y quedo a disposición para proporcionar cualquier información adicional que considere necesaria.\n\n" +
+           "Agradezco de antemano su tiempo y atención. Quedo atento a su respuesta.\n\n" +
+           "Atentamente,\nIng. José Escamilla\nTel: 477 587 1596";
+
   
-    var attachment = DriveApp.getFileById("1Fwd29QE5iVQLlpGCCC1VCwrkXb3Bj30M"); // Reemplaza con el ID del archivo en Google Drive, el ID del archivo es la parte entre /d/ y /view. 
+    var attachment = DriveApp.getFileById("1wQ0LOqxKw1nbckrwYXmVCYrzz_noqtpV"); // Reemplaza con el ID del archivo en Google Drive, el ID del archivo es la parte entre /d/ y /view. 
   
     // Define las fechas de envío
-    var dates = ["2024-09-02", "2024-10-02", "2024-11-02", "2025-12-02", "2025-01-02", "2025-02-02", "2025-03-02", "2025-04-02", "2025-05-02", "2025-06-02", "2025-07-02", "2025-08-02", "2025-09-02"];
+    var dates = ["2025-01-03", "2025-02-03", "2025-03-03", "2025-04-03", "2025-05-03", "2025-06-03", "2025-07-03", "2025-08-03", "2025-09-03", "2025-10-03", "2025-11-03", "2025-12-03"];
     var today = new Date();
     var todayString = Utilities.formatDate(today, Session.getScriptTimeZone(), "yyyy-MM-dd");
     
